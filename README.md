c678a1a681c46734dad883c2a97adc94b0144ec0
HeshanKarunaratne <heshan947102@gmail.com>
2023-12-16 22:32:20 +0530

    NavBar added

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ npx create-next-app@latest digital-hippo
 #### Install shadcn
 npx shadcn-ui@latest init
 
+#### Create a button from shadcn
+npx shadcn-ui@latest add button
---
+++ src/components/NavItems.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useState } from "react";
+import NavItem from "./NavItem";
+
+export const NavItems = () => {
+    const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+
+    return (
+        <div className='flex gap-4 h-full'>
+            {PRODUCT_CATEGORIES.map((categort, i) => {
+                const handleOpen = () => {
+                    if (activeIndex === i) {
+                        setActiveIndex(null);
+                    } else {
+                        setActiveIndex(i);
+                    }
+                }
+
+                const isOpen = i === activeIndex;
+
+                return (
+                    <NavItem />
+                )
+            })}
+
+
+        </div>
+    );
+}
